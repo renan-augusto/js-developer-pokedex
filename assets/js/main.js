6d2fb64bedafc081cd070dbf10515eeffe210e02
renan-augusto <renan_acm1992@hotmail.com>
2023-07-30 09:29:39 -0300

    feat(loading) - load add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,64 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const loadingScreen = document.getElementById("loadingScreen");
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 500;
+const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    return `
+  return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types
+                      .map((type) => `<li class="type ${type}">${type}</li>`)
+                      .join("")}
                 </ol>
 
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
         </li>
-    `
+    `;
 }
 
 function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+  return pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join("");
+    pokemonList.innerHTML += newHtml;
+  });
 }
 
-loadPokemonItens(offset, limit)
+function showLoadingScreen() {
+  loadingScreen.style.display = "block";
+}
 
-loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+function hideLoadingScreen() {
+  loadingScreen.style.display = "none";
+}
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+loadPokemonItens(offset, limit);
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
-    }
-})
+loadMoreButton.addEventListener("click", () => {
+  showLoadingScreen();
+
+  offset += limit;
+  const qtdRecordsWithNextPage = offset + limit;
+
+  if (qtdRecordsWithNextPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit)
+      .then(() => {
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+      })
+      .finally(() => hideLoadingScreen());
+  } else {
+    loadPokemonItens(offset, limit)
+      .then(() => {})
+      .finally(() => hideLoadingScreen());
+  }
+});
